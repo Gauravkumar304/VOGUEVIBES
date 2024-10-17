bc6e9634c2308f9af8c0d0203b3a30b63f5c03a5
Gauravkumar304 <pushkarkumarsingh7499@gmail.com>
2024-10-17 22:13:01 +0530

    first commit

--- VogueVibes/vite.config.js
+++ VogueVibes/vite.config.js
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+        onwarn(warning, warn) {
+            // Ignore specific warnings or handle them
+            if (warning.code === 'THIS_IS_UNDEFINED') return;
+            // Call default handler
+            warn(warning);
+        },
+    },
+},
   server: {port: 5173}
 })
