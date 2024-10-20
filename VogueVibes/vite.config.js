39983932b2fc1002d0b96fd9b51fdd870ee45f61
Gauravkumar304 <pushkarkumarsingh7499@gmail.com>
2024-10-20 19:31:30 +0530

    first commit

--- VogueVibes/vite.config.js
+++ VogueVibes/vite.config.js
@@ -4,13 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port: 5173}
+  server: { port: 5173 },
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom','react-router-dom'], // Replace 'your-external-module' with the actual module name causing the issue
+    },
+  },
 })
+
+
 
 
 // build: {
 //   rollupOptions: {
-//       external: ['react-router-dom'],
+//     external: ['react', 'react-dom','react-router-dom'], // Replace 'your-external-module' with the actual module name causing the issue
 //   },
 // },
 
