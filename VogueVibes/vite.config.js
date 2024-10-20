6b4e9ec229d9dcb4b10db6aa99ec0a8670b90e24
Gauravkumar304 <pushkarkumarsingh7499@gmail.com>
2024-10-20 22:06:20 +0530

    first commit

--- VogueVibes/vite.config.js
+++ VogueVibes/vite.config.js
@@ -1,26 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-//import viteCss from 'vite-plugin-css';  // added line
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],  //viteCss()
+  plugins: [react()],  
   server: { port: 5173 },
   optimizeDeps: {
     include: ['react-toastify/dist/ReactToastify.css','react', 'react-dom','react-router-dom','react-toastify'], // Replace with the problematic module name
   },
-  // assetsInclude: ['**/*.css'],
   build: {
     sourcemap: true,
     rollupOptions: {
       external: ['react', 'react-dom','react-router-dom','react-toastify'], // Replace 'your-external-module' with the actual module name causing the issue
-      onwarn(warning, warn) {
-        if (warning.code === 'UNRESOLVED_IMPORT') {
-          // Skip unresolved import warnings
-          return;
-        }
-        warn(warning);
-      }
     },
   },
 })
